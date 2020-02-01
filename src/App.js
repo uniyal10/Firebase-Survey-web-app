84b55172aea26eb4b27118ebcf598109070814a5
sudhanshu uniyal <uniyal1001@gmail.com>
2020-02-02 00:54:43 +0530

    adding css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import './style.css';
 var firebase=require('firebase')
 var uuid=require('uuid')
 
@@ -15,7 +16,6 @@ var uuid=require('uuid')
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
-
 
 
 class  App extends Component {
@@ -37,11 +37,34 @@ class  App extends Component {
     this.setState({studentName:this.refs.name.value},()=>{console.log(this.state )})
 
   }
+  handleSelected=(e)=>{
+    var answers=this.state.answers;
+    if(e.target.name == 'answer1'){
+      answers.answer1=e.target.value;
+    
+    }
+    else if(e.target.name == 'answer2'){
+      answers.answer2 = e.target.value;
+     
+
+    }
+    else if(e.target.name == 'answer3'){
+      answers.answer3 = e.target.value;
+    
+    }
+    this.setState({answers:answers},()=>{
+      console.log(this.state);
+    })
+  }
+  questionSubmit=(e)=>{
+    e.preventDefault();
+    this.setState({isSubmitted:true})
+  }
   render(){
   var studentName;
   var questions;
   if(this.state.studentName === '' && this.state.isSubmitted === false){
-    studentName = <div>
+    studentName = <div className="studentInfo">
     <h1>hey student, please let us know your name:</h1>
     <form onSubmit={this.handleSubmit}>
       <input type="text" placeholder="name.." ref="name" />
@@ -49,34 +72,33 @@ class  App extends Component {
     </div>
     questions='';
   }
-  else if(this.state.studentName != ''){
+  else if(this.state.studentName != '' && this.state.isSubmitted==false){
     studentName=<h1> hey, {this.state.studentName}</h1>
-    questions=<div>
+    questions=<div className="feedback">
     <h2>Here are some questions</h2>
-    <form>
-    <div>
+    <form onSubmit={this.questionSubmit}>
+    <div className="card">
        <label>What kind of courses you like the most:</label><br />
-       <input type="radio" name="answer1" value="technology" onChange={this.handleSelected} />Technology
-       <input type="radio" name="answe1" value="Design" onChange={this.handleSelected} />Design
-       <input type="radio" name="answer1" value="Marketing" onChange={this.handleSelected} />Marketing
+       <input class="with-gap" type="radio" name="answer1" value="technology" onChange={this.handleSelected} />Technology
+       <input class="with-gap" type="radio" name="answer1" value="design" onChange={this.handleSelected} />Design
+       <input class="with-gap" type="radio" name="answer1" value="marketing" onChange={this.handleSelected} />Marketing
     </div>
-    <div>
+    <div className="card">
 
           <label>You are a:</label><br />
-       <input type="radio" name="answer2" value="student" onChange={this.handleSelected} />Technology
-       <input type="radio" name="answer2" value="In-job" onChange={this.handleSelected} />Design
-       <input type="radio" name="answer2" value="looking-for" onChange={this.handleSelected} />Marketing
-
+       <input type="radio" name="answer2" value="student" onChange={this.handleSelected} />student
+       <input type="radio" name="answer2" value="in-job" onChange={this.handleSelected} />In-job
+       <input type="radio" name="answer2" value="looking-for" onChange={this.handleSelected} />looking-for
     </div>
-    <div>
+    <div className="card">
 
           <label>Is online learning helpful : </label><br />
-       <input type="radio" name="answer3" value="yes" onChange={this.handleSelected} />Technology
-       <input type="radio" name="answer3" value="no" onChange={this.handleSelected} />Design
-       <input type="radio" name="answer3" value="maybe" onChange={this.handleSelected} />Marketing
+          <input type="radio" name="answer3" value="yes" onChange={this.handleSelected} />yes
+          <input type="radio" name="answer3" value="no" onChange={this.handleSelected} />no
+          <input type="radio" name="answer3" value="maybe" onChange={this.handleSelected} />maybe
 
     </div>
-    <input type="submit" value="submit"/>
+    <input className="button" type="submit" value="submit"/>
     </form>
     </div>
   }
